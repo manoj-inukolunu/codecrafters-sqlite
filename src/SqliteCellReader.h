9ff959f239729b9a09b0d5151eed774a148a2c92
Manoj Inukolunu <minukolunu@salesforce.com>
2025-10-02 12:32:08 +0530

    LOcal CHanges

---
+++ src/SqliteCellReader.h
@@ -0,0 +1,18 @@
+//
+// Created by Manoj Inukolunu on 02/10/25.
+//
+
+#ifndef SQLITE_STARTER_CPP_SQLITECELLREADER_H
+#define SQLITE_STARTER_CPP_SQLITECELLREADER_H
+
+#include "types.h"
+
+class SqliteCellReader {
+
+    varint recordSize;
+    varint rowId;
+
+};
+
+
+#endif //SQLITE_STARTER_CPP_SQLITECELLREADER_H
---
+++ src/types.h
@@ -0,0 +1,11 @@
+//
+// Created by Manoj Inukolunu on 02/10/25.
+//
+
+#ifndef SQLITE_STARTER_CPP_TYPES_H
+#define SQLITE_STARTER_CPP_TYPES_H
+
+#endif //SQLITE_STARTER_CPP_TYPES_H
+
+
+typedef long varint;
